5e4e299231d72db93460d9f6096b4ee532ea933b
David J. H. Shih <djh.shih@gmail.com>
2016-12-22 23:15:30 -0500

    Initial implementation.

--- multikey.cpp
+++ multikey.cpp
@@ -1,70 +1,16 @@
 #include <Rcpp.h>
-using namespace Rcpp;
 
 #include <vector>
+//#include <algorithm>
+// Ideally, we would use <algorithm>, but the vector iterator must be typed, 
+// and the types of the sorted vectors contained in x and y are not known
+// at compile time...
+// The auto feature of C++11 may circumvent this issue.
 using namespace std;
 
-template <typename T>
-size_t binary_search(const vector<T>& ys, const T& x) {
-	size_t l = 0;
-	size_t r = ys.size();
-	const vector<T>& ys
-	while (l < r) {
-		size_t m = l + (r - l) / 2;
-		if (ys[m] == x) {
-			return m;
-		} else if (ys[m] < x) {
-			// search right half
-			l = m + 1;
-		} else {
-			// search left half
-			r = m;
-		}
-	}
-	
-	// element not found
-	return ys.size();
-}
+#include "algorithm/vector.hpp"
 
-// return the first index of sorted vector ys that matches x, or if none exists, where x should be inserted
-template <typename T>
-size_t lower_bound(const vector<T>& ys, const T& x) {
-	size_t l = 0;
-	size_t r = ys.size();
-	
-	while (l < r) {
-		size_t m = l + (r - l) / 2;
-		if (ys[m] < x) {
-			// search right half
-			l = m + 1;
-		} else {
-			// search left half
-			r = m;
-		}
-	}
-	
-	return l;
-}
-
-// return one past the last index of sorted vector ys that matches x, or if none exists, where x should be inserted
-template <class T>
-size_t upper_bound(const vector<T>& ys, const T& x) {
-	size_t l = 0;
-	size_t r = ys.size();
-	
-	while (l < r) {
-		size_t m = l + (r - l) / 2;
-		if (x < ys[m]) {
-			// search left half
-			r = m;
-		} else {
-			// search right half
-			l = m + 1;
-		}
-	}
-
-	return l;
-}
+// Rcpp::List is a heterogeneous container. As far as I know, there is no equivalent type in C++.
 
 //' Determine the indices into the reference list for each multi-value element of the query list.
 //' Each element in query and reference lists can be multi-valued. The reference list must be sorted.
@@ -72,24 +18,24 @@ size_t upper_bound(const vector<T>& ys, const T& x) {
 //' @param y  sorted reference list of vectors
 //' @return indices of y for each multi-value element in x
 // [[Rcpp::export]]
-IntegerVector match_sorted_lists(List x, List y) {
+Rcpp::IntegerVector match_sorted_lists(Rcpp::List x, Rcpp::List y) {
 
-	// number of keys
+	// number of components in the key
 	size_t k = x.size();
 	
 	if (k == 0) {
-		stop("List x cannot be empty.");
+		Rcpp::stop("List x cannot be empty.");
 	}
 	
 	if (y.size() != k) {
-		stop("List x and List y must have the same number of keys.");
+		Rcpp::stop("List x and List y must have the same number of keys.");
 	}
 
 	// number of multi-value elements in x
 	size_t nx = x[0].size();
 	for (size_t i = 1; i < k; ++i) {
 		if (nx != x[i].size()) {
-			stop("Each vector in List x must be same size");
+			Rcpp::stop("Each vector in List x must be same size.");
 		}
 	}
 	
@@ -97,18 +43,41 @@ IntegerVector match_sorted_lists(List x, List y) {
 	size_t ny = y[0].size();
 	for (size_t i = 1; i < k; ++i) {
 		if (ny != y[i].size()) {
-			stop("Each vector in List y must be the same size");
+			Rcpp::stop("Each vector in List y must be the same size.");
 		}
 	}
 	
+	Rcpp::IntegerVector idx(nx);
+	
 	// iterate through the multi-value elements of x
 	for (size_t j = 0; j < nx; ++j) {
-		size_t i = 0;
-		size_t ylb = lower_bound(y[i], x[i][j]);
-		size_t yub = upper_bound(y[i], x[i][j]);
-		// TODO
+		// each component of the multi-value key stricts the feasible range [lower, upper) of ys containing elements that possibly match x
+		// if the search completes successfully after the last component, lower points to a possible match
+		size_t lower = lower_bound(y[0], x[0][j]);
+		size_t upper = upper_bound(y[0], x[0][j]);
+		bool match = TRUE;
+		if (y[i][lower] != x[i][j]) {
+			// component of key does not match
+			match = FALSE;	
+		} else {
+			for (size_t i = 1; i < k; ++i) {
+				lower = lower_bound(y[i], x[i][j], lower, upper)
+				upper = upper_bound(y[i], x[i][j], lower, upper)
+				if (y[i][lower] != x[i][j]) {
+					// component of key does not match
+					match = FALSE;
+					break;
+				}
+			}
+		}
+	
+		if (match) {
+			idx[j] = lower;
+		} else {
+			// indicates no match in y for the j-th multi-value element of x
+			idx[j] = ny;
+		}
 	}
-
 
 	return idx;
 }
