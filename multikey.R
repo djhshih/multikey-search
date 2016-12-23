d0c62ffbabfca88239d38ad80f01b538313e204e
David J. H. Shih <djh.shih@gmail.com>
2016-12-23 16:21:12 -0500

    Use slices to avoid copying.

--- multikey.R
+++ multikey.R
@@ -4,47 +4,66 @@ sourceCpp("multikey.cpp");
 
 x <- data.frame(
 	chrom = c(4, 3, 2),
-	pos = c(100, 200, 100)
+	pos = c(1000, 2000, 1000)
 );
 
 y <- data.frame(
 	chrom = c(1, 2, 3, 4),
-	pos = c(100, 100, 200, 200)
+	pos = c(1000, 1000, 2000, 2000)
 );
 
+truth <- c(NA, 3, 2);
 
-#' @param x  data.frame
-#' @param y  data.frame
+as_integer_matrix <- function(x) {
+	if (!is.matrix(x)) {
+		x <- as.matrix(x);
+	}
+	if (length(x) > 1 && class(x[1]) != "integer") {
+		x <- matrix(as.integer(x), nrow=nrow(x));
+	}
+	x
+}
+
+#' Match coordinates.
+#'
+#' @param x  integer matrix
+#' @param y  integer matrix
+#' @return row index into y for each row of x
+#' @export
 match_coords <- function(x, y) {
 
-	if (is.data.frame(x)) {
-		x <- as.list(x);
+	x <- as_integer_matrix(x);
+	y <- as_integer_matrix(y);
+
+	# number of coordinate dimensions
+	d <- ncol(x);
+
+	if (ncol(y) != d) {
+		stop("x and y must have the same coordinate dimensions");
 	}
 
-	if (is.data.frame(y)) {
-		y <- as.list(y);
-	}
+	y.order <- order(y[, 1], y[, 2]);
+	y.sorted <- y[y.order, ];
 
-	k <- length(x);
-
-	if (length(y) != k) {
-		stop("lists x and y must have the same size");
-	}
-
-	y <- y[order(y[[1]], y[[2]])];
-
-	idx <- match_sorted_coords(x, y, k);
+	idx <- match_rows(x, y.sorted);
 
 	# convert 0-based to 1-based
 	idx <- idx + 1
 
 	# recode missing matches
-	idx[idx > length(y[[1]])] <- NA;
+	idx[idx > nrow(y)] <- NA;
 
 	idx
 }
 
 idx <- match_coords(x, y);
+
+
+test_equal <- function(x, y) {
+	all(is.na(x) == is.na(y)) && all(x == y, na.rm=TRUE)
+}
+
+stopifnot(test_equal(idx, truth));
 
 cbind(x, y[idx, ])
 
--- multikey.cpp
+++ multikey.cpp
@@ -1,5 +1,6 @@
 #include <Rcpp.h>
 
+#include <cstdint>
 #include <stdexcept>
 #include <vector>
 //#include <algorithm>
@@ -9,94 +10,36 @@
 // The auto feature of C++11 may circumvent this issue.
 using namespace std;
 
-#include "algorithm/vector.hpp"
+#include "algorithm/match.hpp"
+using namespace alg;
 
-template <typename T>
-void _match_sorted_lists(
-	const vector< vector<T> >& x,
-	const vector< vector<T> >& y,
-	vector<size_t>& idx
-) {
-
-	// number of components in the key
-	size_t k = x.size();
-	
-	if (k == 0) {
-		throw invalid_argument("List x cannot be empty.");
-	}
-	
-	if (y.size() != k) {
-		throw invalid_argument("List x and List y must have the same number of keys.");
-	}
-
-	// number of multi-value elements in x
-	size_t nx = x[0].size();
-	for (size_t i = 1; i < k; ++i) {
-		if (nx != x[i].size()) {
-			throw invalid_argument("Each vector in List x must be same size.");
-		}
-	}
-	
-	// number of multi-value elements in y
-	size_t ny = y[0].size();
-	for (size_t i = 1; i < k; ++i) {
-		if (ny != y[i].size()) {
-			throw invalid_argument("Each vector in List y must be the same size.");
-		}
-	}
-
-	idx.resize(nx);
-	
-	// iterate through the multi-value elements of x
-	for (size_t j = 0; j < nx; ++j) {
-		// each component of the multi-value key stricts the feasible range [lower, upper) of ys containing elements that possibly match x
-		// if the search completes successfully after the last component, lower points to a possible match
-		size_t lower = vec::lower_bound(y[0], x[0][j]);
-		size_t upper = vec::upper_bound(y[0], x[0][j]);
-		bool match = TRUE;
-		if (y[0][lower] != x[0][j]) {
-			// component of key does not match
-			match = FALSE;	
-		} else {
-			for (size_t i = 1; i < k; ++i) {
-				lower = vec::lower_bound(y[i], x[i][j], lower, upper);
-				upper = vec::upper_bound(y[i], x[i][j], lower, upper);
-				if (y[i][lower] != x[i][j]) {
-					// component of key does not match
-					match = FALSE;
-					break;
-				}
-			}
-		}
-	
-		if (match) {
-			idx[j] = lower;
-		} else {
-			// indicates no match in y for the j-th multi-value element of x
-			idx[j] = ny;
-		}
-	}
-}
-
-
-//' Determine the indices into the reference list for each multi-value element of the query list.
-//' Each element in query and reference lists can be multi-valued. The reference list must be sorted.
-//' @param x  query list of sorted vectors, in which the i-th element of each vector constitute the i-th multi-value element
-//' @param y  sorted reference list of vectors
-//' @return indices of y for each multi-value element in x
+//' Determine the row index into row-sorted y for each row of x.
+//' 
+//' The rows of y must be sorted by considering each row as a mutli-value key,
+//' with the most significant key in the first column.
+//'
+//' @param x  query integer matrix
+//' @param y  reference row-sorted integer matrix
+//' @return row index into y for each row fo x
 // [[Rcpp::export]]
-Rcpp::IntegerVector match_sorted_coords(Rcpp::List x, Rcpp::List y, size_t k) {
+Rcpp::IntegerVector match_rows(Rcpp::IntegerMatrix x, Rcpp::IntegerMatrix y) {
 
-	vector< vector<long> > vx(k);
-	vector< vector<long> > vy(k);
-	vector<size_t> idx;
-	
-	for (size_t i = 0; i < k; ++i) {
-		vx[i] = Rcpp::as< vector<long> >(x[i]);
-		vy[i] = Rcpp::as< vector<long> >(y[i]);
+	size_t nx = x.nrow();
+	size_t d = x.ncol();
+
+	vector< slice<int32_t> > vx;
+	vector< slice<int32_t> > vy;
+	vector<size_t> idx(nx);
+
+	vx.reserve(d);
+	vy.reserve(d);
+
+	for (size_t j = 0; j < d; ++j) {
+		vx.push_back( slice<int32_t>(x.column(j).begin(), x.column(j).end()) );
+		vy.push_back( slice<int32_t>(y.column(j).begin(), y.column(j).end()) );
 	}
 
-	_match_sorted_lists(vx, vy, idx);
+	match_slices(vx, vy, idx);
 
 	return Rcpp::wrap(idx);
 }
